71718a0c71f9aa64c03389f40a245be7f597c31a
Johan Nilsson <github@markupartist.com>
2013-04-14 18:03:02 +0200

    Change to SPI.

--- src/arduino/letterweight/letterweight.ino
+++ src/arduino/letterweight/letterweight.ino
@@ -1,106 +1,119 @@
 /*
- Attached an Serial7Segment to an Arduino Uno using the following pins:
- A5 to SCL
- A4 to SDA
- VIN to PWR
- GND to GND
+The Weight of Words
+
+https://github.com/johannilsson/viktenavord
+Johan Nilsson johan@markupartist.com
+
+
+Serial 7-Segment Display from Sparkfun
+
+   Circuit:
+   Arduino -------------- Serial 7-Segment
+     5V   --------------------  VCC
+     GND  --------------------  GND
+      8   --------------------  SS
+     11   --------------------  SDI
+     13   --------------------  SCK
+     
+
+Based on Jim Lindbloms spi example;
+https://www.sparkfun.com/tutorials/407
+
 */
+#include <SPI.h> // Include the Arduino SPI library
 
-#include <Wire.h>
+const int ssPin = 8;
+unsigned int weight = 0;
 
-#define DISPLAY_ADDRESS1 0x71 //This is the default address of the OpenSegment with both solder jumpers open
-
-int weight = 0;
+char tempString[10];  // Will be used with sprintf to create strings
 
 void setup() {
-  Wire.begin();
-
   Serial.begin(9600);
 
-  //Send the reset command to the display - this forces the cursor to return to the beginning of the display
-  Wire.beginTransmission(DISPLAY_ADDRESS1);
-  Wire.write('v');
-  Wire.endTransmission();
+  pinMode(ssPin, OUTPUT);
+  digitalWrite(ssPin, HIGH);
+  SPI.begin();
+  SPI.setClockDivider(SPI_CLOCK_DIV64);  // Slow down SPI clock
 
-  setBrightness();
+  clearDisplaySPI(); // Clear and reset.
+
+  setString("boot");
+  delay(500);
+  //setDecimalsSPI(0b111111);  // Turn on all decimals, colon, apos
+  //delay(500);
+  //setDecimalsSPI(0b00000010);  // Sets digit 3 decimal on
+  //setBrightnessSPI(0);  // Lowest brightness
+  setBrightnessSPI(255);  // High brightness
+
+  clearDisplaySPI();  
 }
 
 void loop() {
-  if (Serial.available()) { // If data is available to read,
+  clearDisplaySPI();
+
+  if (Serial.available()) {
     weight = Serial.read();    
   }
 
-
- // Print the decimal at the proper spot
-  //if (weight < 100)
-  setDecimalsI2C(0b00000100);  // Sets digit 3 decimal on
-  //else
-  //  setDecimalsI2C(0b00001000);
-
-  // todo, change to display left to right.
-
-  i2cSendValue(weight);
-   
-  delay(100);
-}
-
-void setBrightness() {
-  Wire.beginTransmission(DISPLAY_ADDRESS1);
-  Wire.write(0x7A); // Brightness control command
-  Wire.write(50); // Set brightness level: 0% to 100%
-  Wire.endTransmission();
-  i2cSendString("boot");
-  delay(2000);
-}
-
-/*
-  Given a number, i2cSendValue chops up an integer into 
-  four values and sends them out over I2C
-*/
-void i2cSendValue(int tempCycles) {
-  Wire.beginTransmission(DISPLAY_ADDRESS1); // transmit to device #1
-
-  Wire.write(tempCycles / 1000); //Send the left most digit
-  tempCycles %= 1000; //Now remove the left most digit from the number we want to display  
-  Wire.write(tempCycles / 100);
-  tempCycles %= 100;
-  Wire.write(tempCycles / 10);
-  tempCycles %= 10;
-  Wire.write(tempCycles); //Send the right most digit
-  Wire.endTransmission(); //Stop I2C transmission
-}
-
-//Given a string, i2cSendString chops up the string and sends out the first four characters over i2c
-void i2cSendString(char *toSend) {
-  Wire.beginTransmission(DISPLAY_ADDRESS1); // transmit to device #1
-  for(byte x = 0 ; x < 4 ; x++)
-    Wire.write(toSend[x]); //Send a character from the array out over I2C
-  Wire.endTransmission(); //Stop I2C transmission
+  //sprintf(tempString, "%4d", weight);
+  setInt(weight);
+  
+  delay(100);  // This will make the display update at 100Hz.*/
 }
 
 // This custom function works somewhat like a serial.print.
 //  You can send it an array of chars (string) and it'll print
 //  the first 4 characters in the array.
-void sendStringI2C(String toSend) {
-  Wire.beginTransmission(DISPLAY_ADDRESS1);
+void setString(String toSend) {
+  digitalWrite(ssPin, LOW);
   for (int i=0; i<4; i++) {
-    Wire.write(toSend[i]);
+    SPI.transfer(toSend[i]);
   }
-  Wire.endTransmission();
+  digitalWrite(ssPin, HIGH);
+}
+
+void setInt(int tempCycles) {
+  digitalWrite(ssPin, LOW);
+
+  SPI.transfer(tempCycles / 1000);
+  tempCycles %= 1000;
+  SPI.transfer(tempCycles / 100);
+  tempCycles %= 100;
+  SPI.transfer(tempCycles / 10);
+  tempCycles %= 10;
+  SPI.transfer(tempCycles);
+
+  digitalWrite(ssPin, HIGH);
+}
+
+// Send the clear display command (0x76)
+//  This will clear the display and reset the cursor
+void clearDisplaySPI() {
+  digitalWrite(ssPin, LOW);
+  SPI.transfer(0x76);  // Clear display command
+  digitalWrite(ssPin, HIGH);
+}
+
+// Set the displays brightness. Should receive byte with the value
+//  to set the brightness to
+//  dimmest------------->brightest
+//     0--------127--------255
+void setBrightnessSPI(byte value) {
+  digitalWrite(ssPin, LOW);
+  SPI.transfer(0x7A);  // Set brightness command byte
+  SPI.transfer(value);  // brightness data byte
+  digitalWrite(ssPin, HIGH);
 }
 
 // Turn on any, none, or all of the decimals.
 //  The six lowest bits in the decimals parameter sets a decimal 
 //  (or colon, or apostrophe) on or off. A 1 indicates on, 0 off.
 //  [MSB] (X)(X)(Apos)(Colon)(Digit 4)(Digit 3)(Digit2)(Digit1)
-void setDecimalsI2C(byte decimals) {
-  // 0b00001000
-  // 
-  Wire.beginTransmission(DISPLAY_ADDRESS1);
-  Wire.write(0x77);
-  Wire.write(decimals);
-  Wire.endTransmission();
+void setDecimalsSPI(byte decimals) {
+  digitalWrite(ssPin, LOW);
+  SPI.transfer(0x77);
+  SPI.transfer(decimals);
+  digitalWrite(ssPin, HIGH);
 }
-
 
 
